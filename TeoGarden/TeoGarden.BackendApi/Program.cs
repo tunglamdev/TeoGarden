2cb51f47192682e35f92b121fbdcc39756a712ac
tunglamctu <86045295+tunglamctu@users.noreply.github.com>
2022-06-25 01:46:35 +0700

    Create: TeoGarden.BackendApi for API
    Using swagger for API testing

--- TeoGarden/TeoGarden.BackendApi/Program.cs
+++ TeoGarden/TeoGarden.BackendApi/Program.cs
@@ -1,10 +1,35 @@
+using System.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using TeoGarden.Data.EF;
+using Microsoft.Extensions.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "CORS",
+                      policy =>
+                      {
+                          policy.AllowAnyOrigin()
+                          .AllowAnyMethod()
+                          .AllowAnyHeader();
+                      });
+});
+
+builder.Services.AddDbContext<TeoGardenDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
+
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "TeoGarden.Api", Version = "v1" });
+});
 
 var app = builder.Build();
 
@@ -12,8 +37,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TeoGarden.BackendApi v1"));
 }
+
+app.UseCors("CORS"); 
 
 app.UseHttpsRedirection();
 
