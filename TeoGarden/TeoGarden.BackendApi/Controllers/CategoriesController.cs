e8510a10f4e471f205c3bd31ea4da8a09ed86475
tunglamctu <86045295+tunglamctu@users.noreply.github.com>
2022-06-25 01:54:19 +0700

    Create: CategoriesController and VegetablesController for the first API testing

---
+++ TeoGarden/TeoGarden.BackendApi/Controllers/CategoriesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TeoGarden.Data.EF;
+
+namespace TeoGarden.BackendApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly TeoGardenDbContext _context;
+        public CategoriesController(TeoGardenDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var categories = _context.Categories.ToList();
+            return Ok(categories);
+        }
+    }
+}
---
+++ TeoGarden/TeoGarden.BackendApi/Controllers/VegetablesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TeoGarden.Data.EF;
+
+namespace TeoGarden.BackendApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VegetablesController : ControllerBase
+    {
+        private readonly TeoGardenDbContext _context;
+        public VegetablesController(TeoGardenDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var vegetables = _context.Vegetables.ToList();
+            return Ok(vegetables);
+        }
+    }
+}
