74a753362ed5c1c5880f7432b8e3b0b1deb1d853
tunglamctu <86045295+tunglamctu@users.noreply.github.com>
2022-06-29 23:57:45 +0700

    Add: TeoGarden.Sdk for using Refit to call API from customersite

---
+++ TeoGarden/TeoGarden.Sdk/Services/ICategoryApi.cs
@@ -0,0 +1,11 @@
+﻿using Refit;
+using TeoGarden.ShareModel.ViewModels;
+
+namespace TeoGarden.Sdk.Services
+{
+    public interface ICategoryApi
+    {
+        [Get("/api/Categories")]
+        Task<List<CategoryViewModel>> GetAllAsync();
+    }
+}
---
+++ TeoGarden/TeoGarden.Sdk/Services/IVegetableApi.cs
@@ -0,0 +1,19 @@
+﻿using Refit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeoGarden.ShareModel.ViewModels;
+
+namespace TeoGarden.Sdk.Services
+{
+    public interface IVegetableApi
+    {
+        [Get("/api/Vegetables")]
+        Task<List<VegetableViewModel>> GetAllAsync();
+
+        [Get("/api/Vegetables/Category/Id")]
+        Task<List<VegetableViewModel>> GetByCategoryAsync(int Id);
+    }
+}
