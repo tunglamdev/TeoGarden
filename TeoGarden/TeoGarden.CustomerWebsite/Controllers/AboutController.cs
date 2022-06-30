a30cfc5b5167f442eccf525112eeb281d7e5532d
tunglamctu <86045295+tunglamctu@users.noreply.github.com>
2022-06-30 23:57:13 +0700

    Add: AboutController

---
+++ TeoGarden/TeoGarden.CustomerWebsite/Controllers/AboutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TeoGarden.CustomerWebsite.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
