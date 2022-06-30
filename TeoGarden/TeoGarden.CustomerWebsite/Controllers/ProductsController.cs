8858db5479fdf3757fb4f139cd88a477ac9bcd85
tunglamctu <86045295+tunglamctu@users.noreply.github.com>
2022-06-30 23:56:36 +0700

    Update: ProductsController for getting details and finding by keywords

--- TeoGarden/TeoGarden.CustomerWebsite/Controllers/ProductsController.cs
+++ TeoGarden/TeoGarden.CustomerWebsite/Controllers/ProductsController.cs
@@ -7,12 +7,12 @@ namespace TeoGarden.CustomerWebsite.Controllers
 {
     public class ProductsController : Controller
     {
-        private IVegetableApi _vegetableApi;
-        private HomeViewModel _viewData;
+        private readonly IVegetableApi _vegetableApi;
+        private readonly ProductViewModel _viewData;
         public ProductsController()
         {
             _vegetableApi = RestService.For<IVegetableApi>("https://localhost:7116");
-            _viewData = new HomeViewModel();
+            _viewData = new ProductViewModel();
         }
 
         public async Task<IActionResult> Index()
@@ -26,6 +26,23 @@ namespace TeoGarden.CustomerWebsite.Controllers
         {
             var vegetables = _vegetableApi.GetByCategoryAsync(Id).GetAwaiter().GetResult();
             _viewData.Vegetables = vegetables;
+            return View(_viewData);
+        }
+
+        public async Task<IActionResult> Detail(int Id)
+        {
+            var vegetable = _vegetableApi.GetByIdAsync(Id).GetAwaiter().GetResult();
+            var vegetables = _vegetableApi.GetByCategoryAsync(vegetable.CategoryId).GetAwaiter().GetResult();
+            _viewData.Vegetable = vegetable;
+            _viewData.Vegetables = vegetables;
+            return View(_viewData);
+        }
+
+        public async Task<IActionResult> Search(string Key)
+        {
+            var vegetables = _vegetableApi.FindByKeyAsync(Key).GetAwaiter().GetResult();
+            _viewData.Vegetables = vegetables;
+            _viewData.Keyword = Key;
             return View(_viewData);
         }
     }
