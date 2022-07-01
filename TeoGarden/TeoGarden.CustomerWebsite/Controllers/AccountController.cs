2dde0d7f34f03e148f9d2a85932283ceae35e4c4
tunglamctu <86045295+tunglamctu@users.noreply.github.com>
2022-07-01 22:40:56 +0700

    Add: Views for registering and logging in

---
+++ TeoGarden/TeoGarden.CustomerWebsite/Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TeoGarden.CustomerWebsite.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult Logout()
+        {
+            return View("index", null);
+        }
+    }
+}
